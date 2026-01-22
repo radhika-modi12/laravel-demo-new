c1d4b14faba2ca2cf5c7543df79b18a527e09f71
radhika-modi12 <radhika.h.modi11@gmail.com>
2026-01-22 23:23:17 +0530

    upload new index file

--- api/index.php
+++ api/index.php
@@ -1,2 +1,17 @@
-<?php  
-require __DIR__ . "/../public/index.php";
+<?php
+
+use Illuminate\Http\Request;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);

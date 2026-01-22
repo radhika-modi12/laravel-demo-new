a9a535208cdb982b184c37eafa4cdce9480b137c
radhika-modi12 <radhika.h.modi11@gmail.com>
2026-01-22 18:11:01 +0530

    upload vercel code

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\BenchmarkController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::get('/hello', function () {
+    return response()->json(['message' => 'Hello World!']);
+});
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/users', function () {
+    return response()->json(['users' => User::all()]);
+});
+
+Route::prefix('benchmark')->group(function () {
+    Route::get('/users/{iterations?}', [BenchmarkController::class, 'users']);
 });

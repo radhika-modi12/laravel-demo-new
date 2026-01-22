2a1065261869bdc3847ac6c13a1dbe301c1af219
radhika-modi12 <radhika.h.modi11@gmail.com>
2026-01-22 22:51:50 +0530

    upload new vercel code

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,7 @@
 <?php
 
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::domain('{project}.localhost')->group(function () {
-    Route::get('/', function ($project) {
-        return "Funcionou! Project = $project";
-    });
-});
-
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/users', function () {
-    return User::all();
-});
-
-Route::get('/users/{user}', function (User $user) {
-    return $user;
-})->name('users.show');
